80e02d545a5d2aa617338597dfa36c8b0384040a
Brian Besaw <bjbesaw@outlook.com>
2017-08-25 23:41:53 -0400

    basic functionality working

---
+++ index.js
@@ -0,0 +1,109 @@
+const STORE = [
+  {name: "apples", checked: false},
+  {name: "oranges", checked: false},
+  {name: "milk", checked: true},
+  {name: "bread", checked: false}
+];
+
+// ** GEN ITEM STRING
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+// ** GEN LIST STRING
+function generateShoppingItemsString(shoppingList) {
+  console.log("Generating shopping list element");
+
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  
+  return items.join("");
+}
+
+// ***** RENDER SHOPPING LIST
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+// ** GET ID
+function getItemIndexFromElement(item) {
+  const itemIndexString = $(item)
+    .closest('.js-item-index-element')
+    .attr('data-item-index');
+  return parseInt(itemIndexString, 10);
+}
+
+// ***** ADD ITEM SUBMIT HANDLER
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
+// ** ADD ITEM
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({name: itemName, checked: false});
+}
+
+// ***** CHECK HANDLER
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    toggleCheckedForListItem(itemIndex);
+    renderShoppingList();
+  });
+}
+
+// ** TOGGLE CHECKED
+function toggleCheckedForListItem(itemIndex) {
+  console.log("Toggling checked property for item at index " + itemIndex);
+  STORE[itemIndex].checked = !STORE[itemIndex].checked;
+}
+
+// ***** DELETE HANDLER
+function handleDeleteItemClicked() {
+  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+  console.log('`handleDeleteItemClicked` ran')
+  const itemIndex = getItemIndexFromElement(event.currentTarget);
+  delItemFromList(itemIndex);
+  renderShoppingList();
+  });
+}
+
+// ** DELETE ITEM
+function delItemFromList(itemIndex){
+  console.log('`delItemFromList` ran');
+  STORE.splice(itemIndex,1);
+}
+
+
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+$(handleShoppingList);
