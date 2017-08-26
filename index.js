941c14f5b25c3f0f17d00ad79ecc737238f93c71
Brian Besaw <bjbesaw@outlook.com>
2017-08-26 00:13:29 -0400

    modified store structure

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-// I changed this file on branch.
-
-const STORE = [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
-];
+const STORE = {
+  itemList: 
+  [ {name: "apples", price: 1.19, checked: false},
+    {name: "oranges", price: 1.79, checked: false},
+    {name: "milk", price: 2.49, checked: true},
+    {name: "bread", price: 1.29, checked: false}
+  ],
+  editItem: null,       // null or item no
+  delItem: null,        // null or item no
+  searchTxt: null,      // null or search text
+  filterApp: 'all',     // 'all', 'ch', or 'unCh'
+  sortApp: null         // null, 'hiLo', or 'loHi'
+};
 
 // ** GEN ITEM STRING
 function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">
+        ${item.name}:  ${item.price}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -36,7 +42,7 @@ function generateShoppingItemsString(shoppingList) {
 function renderShoppingList() {
   // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
-  const shoppingListItemsString = generateShoppingItemsString(STORE);
+  const shoppingListItemsString = generateShoppingItemsString(STORE.itemList);
 
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
@@ -65,7 +71,7 @@ function handleNewItemSubmit() {
 // ** ADD ITEM
 function addItemToShoppingList(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
-  STORE.push({name: itemName, checked: false});
+  STORE.itemList.push({name: itemName, checked: false});
 }
 
 // ***** CHECK HANDLER
@@ -81,7 +87,7 @@ function handleItemCheckClicked() {
 // ** TOGGLE CHECKED
 function toggleCheckedForListItem(itemIndex) {
   console.log("Toggling checked property for item at index " + itemIndex);
-  STORE[itemIndex].checked = !STORE[itemIndex].checked;
+  STORE.itemList[itemIndex].checked = !STORE.itemList[itemIndex].checked;
 }
 
 // ***** DELETE HANDLER
@@ -97,7 +103,7 @@ function handleDeleteItemClicked() {
 // ** DELETE ITEM
 function delItemFromList(itemIndex){
   console.log('`delItemFromList` ran');
-  STORE.splice(itemIndex,1);
+  STORE.itemList.splice(itemIndex,1);
 }
 
 
